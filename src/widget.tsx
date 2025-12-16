fcdb3233435374c1743d34d5d94f3174d1d8a59c
tombch <t.brier@outlook.com>
2025-12-16 23:36:56 +0000

    Renamed widget, added s3 presign handler

--- src/widget.tsx
+++ src/widget.tsx
@@ -1,40 +1,35 @@
 import React from 'react';
 import { ReactWidget } from '@jupyterlab/apputils';
+import { requestAPI } from './handler';
 import JupyterIGV from 'jupyter-igv-component';
 
-export class IGVWidget extends ReactWidget {
-  constructor(
-    httpPathHandler: (route: string) => Promise<Response>,
-    s3PathHandler: (path: string) => Promise<void>,
-    fileWriter: (path: string, content: string) => Promise<void>,
-    version: string,
-    name: string
-  ) {
+export class JupyterIGVWidget extends ReactWidget {
+  constructor(version: string, name: string) {
     super();
-    this.addClass('igv-widget');
-    this.httpPathHandler = httpPathHandler;
-    this.s3PathHandler = s3PathHandler;
-    this.fileWriter = fileWriter;
     this.version = version;
     this.name = name;
+
+    // Add class for the widget
+    this.addClass('jupyter-igv-widget');
   }
 
-  httpPathHandler: (route: string) => Promise<Response>;
-  s3PathHandler: (path: string) => Promise<void>;
-  fileWriter: (path: string, content: string) => Promise<void>;
   version: string;
   name: string;
+
+  // Handler for generating presigned S3 URLs
+  s3PresignHandler = async (uri: string): Promise<string> => {
+    const data = await requestAPI<any>('s3-presign', {}, ['uri', uri]);
+    return data['url'];
+  };
 
   render(): JSX.Element {
     return (
       <JupyterIGV
-        httpPathHandler={this.httpPathHandler}
-        s3PathHandler={this.s3PathHandler}
-        fileWriter={this.fileWriter}
-        extVersion={this.version}
+        version={this.version}
+        s3PresignHandler={this.s3PresignHandler}
       />
     );
   }
 }
 
-export default IGVWidget;
+export default JupyterIGVWidget;
